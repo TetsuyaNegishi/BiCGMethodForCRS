9df2233ffc3acc36e892d8ed4ab8d4956d0bb714
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-02-05 09:42:45 +0900

    [update]crsの定義を追加

--- crs_bicg.f90
+++ crs_bicg.f90
@@ -1,4 +1,13 @@
+module crs_matrix
+    type crs
+        complex(8), allocatable :: val
+        integer, allocatable :: col_int, row_ptr
+    end type ers
+end module crs_matrix
+
 program main
+    use crs_matrix
+    implicit none
     integer matrix_size
     real(8) gamma
     character :: arg*10
