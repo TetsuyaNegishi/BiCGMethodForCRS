67f4182aa007e03a4647bcb5958bc9dd4251b54b
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-02-05 11:22:37 +0900

    [update]行列初期化するサブルーチン実装

--- crs_bicg.f90
+++ crs_bicg.f90
@@ -20,4 +20,44 @@ program main
 
     print *, matrix_size
     print *, gamma
+
+    call init(matrix, matrix_size, gamma)
+
+    print *, "val=", matrix%val
+    print *, "col=", matrix%col_int
+    print *, "row=", matrix%row_ptr
+
+    deallocate(matrix%val, matrix%col_int, matrix%row_ptr)
+
+contains
+
+    subroutine init(matrix, matrix_size, gamma)
+        type(crs), intent(out) :: matrix
+        integer, intent(in) :: matrix_size
+        real(8), intent(in) :: gamma
+        integer :: i, j
+        integer :: val_num
+
+        val_num = matrix_size * 3 - 2
+        allocate(matrix%val(val_num), matrix%col_int(val_num), matrix%row_ptr(matrix_size + 1))
+
+        j = 1
+        do i = 1, matrix_size
+            matrix%row_ptr(i) = j
+            if (i > 1) then
+                matrix%val(j) = gamma
+                matrix%col_int(j) = i - 1
+                j = j + 1
+            end if
+            matrix%val(j) = 2
+            matrix%col_int(j) = i
+            j = j + 1
+            if (i < matrix_size) then
+                matrix%val(j) = 1
+                matrix%col_int(j) = i + 1
+                j = j + 1
+            end if
+        end do
+        matrix%row_ptr(matrix_size + 1) = j
+    end subroutine init
 end program main
